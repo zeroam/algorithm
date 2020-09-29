e8c1f37171932e128d5ed15f3c11b6715a122fe5
jayone <imdff0803@gmail.com>
2020-09-29 22:07:43 +0900

    leetcode - 399. Evaluate Division - Approach 2: Union-Find with Weights

--- leetcode/399_Evaluate_Division.py
+++ leetcode/399_Evaluate_Division.py
@@ -48,23 +48,76 @@ class SolutionGraph:
         return results
 
 
+class SolutionUnion:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        
+        gid_weights = {}
+        
+        def find(node_id):
+            if node_id not in gid_weights:
+                gid_weights[node_id] = (node_id, 1)
+            group_id, node_weight = gid_weights[node_id]
+            # The above statements are equivalent to the following one
+            # group_id, node_weight = gid_weight.setdefault(node_id, (node_id, 1))
+            
+            if group_id != node_id:
+                # found inconsistency, trigger chain update
+                new_group_id, group_weight = find(group_id)
+                gid_weights[node_id] = (new_group_id, node_weight * group_weight)
+                
+            return gid_weights[node_id]
+        
+        def union(dividend, divisor, value):
+            dividend_gid, dividend_weight = find(dividend)
+            divisor_gid, divisor_weight = find(divisor)
+            if dividend_gid != divisor_gid:
+                # merge the two groups together
+                # by attaching the dividend group to the one of divisor
+                gid_weights[dividend_gid] = (divisor_gid, divisor_weight * value / dividend_weight)
+                
+        # Step 1). build the union groups
+        for (dividend, divisor), value in zip(equations, values):
+            union(dividend, divisor, value)
+            
+        results = []
+        # Step 2). run the evaluation, with "lazy" updates in find() function
+        for (dividend, divisor) in queries:
+            if dividend not in gid_weights or divisor not in gid_weights:
+                # case 1). at least one variable did not appear before
+                results.append(-1.0)
+            else:
+                dividend_gid, dividend_weight = find(dividend)
+                divisor_gid, divisor_weight = find(divisor)
+                if dividend_gid != divisor_gid:
+                    # case 2). the variable do not belong to the same chain/group
+                    results.append(-1.0)
+                else:
+                    # case 3). there is a chain/path between the variables
+                    results.append(dividend_weight / divisor_weight)
+        return results
+
+
 if __name__ == "__main__":
     s_g = SolutionGraph()
+    s_u = SolutionUnion()
 
     equations = [["a", "b"], ["b", "c"]]
     values = [2.0, 3.0]
     queries = [["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"]]
     expect = [6.0000, 0.50000, -1.00000, 1.00000, -1.00000]
     assert s_g.calcEquation(equations, values, queries) == expect
+    assert s_u.calcEquation(equations, values, queries) == expect
 
     equations = [["a", "b"], ["b", "c"], ["bc", "cd"]]
     values = [1.5, 2.5, 5.0]
     queries = [["a", "c"], ["c", "b"], ["bc", "cd"], ["cd", "bc"]]
     expect = [3.75000, 0.40000, 5.00000, 0.20000]
     assert s_g.calcEquation(equations, values, queries) == expect
+    assert s_u.calcEquation(equations, values, queries) == expect
 
     equations = [["a", "b"]]
     values = [0.5]
     queries = [["a", "b"], ["b", "a"], ["a", "c"], ["x", "y"]]
     expect = [0.50000, 2.00000, -1.00000, -1.00000]
     assert s_g.calcEquation(equations, values, queries) == expect
+    assert s_u.calcEquation(equations, values, queries) == expect
