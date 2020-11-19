40567dba1037b5db452352751bc4535c86bd44ca
jayone <imdff0803@gmail.com>
2020-11-19 10:06:10 +0900

    leetcode - 56. Merge Intervals, 2th - 2th solution

--- leetcode/56_Merge_Intervals.py
+++ leetcode/56_Merge_Intervals.py
@@ -20,21 +20,48 @@ class Solution:
         return merged_intervals
 
 
+class Solution2:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x: x[0])
+        merged_list = [intervals[0]]
+
+        for cur_s, cur_e in intervals[1:]:
+            prev_s, prev_e = merged_list[-1]
+            if cur_s <= prev_e:
+                if prev_e < cur_e:
+                    merged_list[-1][1] = cur_e
+            else:
+                merged_list.append([cur_s, cur_e])
+                
+                
+        return merged_list
+
+
+def assert_test(intervals: List[List[int]], expect: List[List[int]]) -> None:
+    assert s.merge(intervals) == expect
+    assert s2.merge(intervals) == expect
+        
+
 if __name__ == "__main__":
     s = Solution()
+    s2 = Solution2()
 
     intervals = [[1, 4], [0, 1]]
     expect = [[0, 4]]
-    assert s.merge(intervals) == expect
+    assert_test(intervals, expect)
 
     intervals = [[1, 4], [0, 4]]
     expect = [[0, 4]]
-    assert s.merge(intervals) == expect
+    assert_test(intervals, expect)
 
     intervals = [[1,3],[2,6],[8,10],[15,18]]
     expect = [[1,6],[8,10],[15,18]]
-    assert s.merge(intervals) == expect
+    assert_test(intervals, expect)
 
     intervals = [[1,4],[4,5]]
     expect = [[1, 5]]
-    assert s.merge(intervals) == expect
+    assert_test(intervals, expect)
+
+    intervals = [[2, 3], [4, 5], [6, 7], [8, 9], [1, 10]]
+    expect = [[1, 10]]
+    assert_test(intervals, expect)
