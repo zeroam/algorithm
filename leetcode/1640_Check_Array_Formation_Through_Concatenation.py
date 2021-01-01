b53cd13d027bd21823878cb76390686bdd5b073e
jayone <imdff0803@gmail.com>
2021-01-01 20:00:49 +0900

    leetcode - 1640, add solutions

--- leetcode/1640_Check_Array_Formation_Through_Concatenation.py
+++ leetcode/1640_Check_Array_Formation_Through_Concatenation.py
@@ -32,11 +32,97 @@ class Solution:
                 return i
         return -1
 
+class SolutionOneByOne:
+    def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
+        n = len(arr)
+        i = 0
+        while i < n:
+            for p in pieces:
+                if p[0] == arr[i]:
+                    break
+            else:
+                return False
+
+            for x in p:
+                if x != arr[i]:
+                    return False
+
+                i += 1
+
+        return True
+
+
+class SolutionBS:
+    def _binary_search(self, arr: List[List[int]], num: int) -> int:
+        n = len(arr)
+
+        left = 0
+        right = n - 1
+        found = -1
+        while left <= right:
+            mid = (left + right) // 2
+            if arr[mid][0] < num:
+                left = mid + 1
+            elif arr[mid][0] > num:
+                right = mid - 1
+            else:
+                found = mid
+                break
+
+        return found
+
+
+    def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
+        n = len(arr)
+        i = 0
+        pieces.sort()
+
+        while i < n:
+            found = self._binary_search(pieces, arr[i])
+            if found == -1:
+                return False
+
+            p = pieces[found]
+            for x in p:
+                if x != arr[i]:
+                    return False
+
+                i += 1
+
+        return True
+
+
+class SolutionHashMap:
+    def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
+        n = len(arr)
+        # initialize hashmap
+        mapping = {p[0]: p for p in pieces}
+
+        i = 0
+        while i < n:
+            if arr[i] not in mapping:
+                return False
+
+            p = mapping[arr[i]]
+            for x in p:
+                if x != arr[i]:
+                    return False
+
+                i += 1
+
+        return True
+
 
 def check(arr: list, pieces: list, expect: bool):
     s = Solution()
+    s_o = SolutionOneByOne()
+    s_bs = SolutionBS()
+    s_hm = SolutionHashMap()
 
     assert s.canFormArray(arr, pieces) == expect
+    assert s_o.canFormArray(arr, pieces) == expect
+    assert s_bs.canFormArray(arr, pieces) == expect
+    assert s_hm.canFormArray(arr, pieces) == expect
 
 
 if __name__ == "__main__":
