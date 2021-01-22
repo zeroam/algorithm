fe44808849d5c531c6ef302d468248cd553eae97
jayone <imdff0803@gmail.com>
2021-01-22 09:04:24 +0900

    leetcode - 102. Binary Tree Level Order Traversal - add solutions for recursion, iteration

--- leetcode/102_Binary_Tree_Level_Order_Traversal.py
+++ leetcode/102_Binary_Tree_Level_Order_Traversal.py
@@ -31,11 +31,65 @@ class Solution:
         return output
 
 
+class SolutionRecursive:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        levels = []
+        if root is None:
+            return levels
+
+        def helper(node: TreeNode, level: int = 0):
+            # start the current level
+            if level == len(levels):
+                levels.append([])
+
+            # append the current node value
+            levels[level].append(node.val)
+
+            # process child nodes for the next level
+            if node.left:
+                helper(node.left, level + 1)
+            if node.right:
+                helper(node.right, level + 1)
+
+        helper(root)
+
+        return levels
+
+
+class SolutionIterative:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        levels = []
+        if root is None:
+            return levels
+
+        level = 0
+        dq = deque([root])
+        while dq:
+            levels.append([])
+
+            level_size = len(dq)
+            for _ in range(level_size):
+                node = dq.popleft()
+                levels[level].append(node.val)
+
+                if node.left:
+                    dq.append(node.left)
+                if node.right:
+                    dq.append(node.right)
+
+            level += 1
+
+        return levels
+
+
 def check_solution(case: List[int], expect: List[List[int]]):
     s = Solution()
+    s_recursive = SolutionRecursive()
+    s_iterative = SolutionIterative()
 
     assert s.levelOrder(make_tree_node(case)) == expect
-
+    assert s_recursive.levelOrder(make_tree_node(case)) == expect
+    assert s_iterative.levelOrder(make_tree_node(case)) == expect
 
 
 if __name__ == "__main__":
