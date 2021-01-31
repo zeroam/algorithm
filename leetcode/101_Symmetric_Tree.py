97ad73ca43c057fed5603e09b7fb7f21128b6423
jayone <imdff0803@gmail.com>
2021-01-31 10:34:16 +0900

    leetcode - 101. Symmetrix Tree - add solution recursive, iterative

--- leetcode/101_Symmetric_Tree.py
+++ leetcode/101_Symmetric_Tree.py
@@ -24,10 +24,48 @@ class Solution:
         return True
 
 
+class SolutionRecur:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        def isMirror(t1: TreeNode, t2: TreeNode) -> bool:
+            if t1 is None and t2 is None:
+                return True
+            if t1 is None or t2 is None:
+                return False
+            return (t1.val == t2.val) and isMirror(t1.left, t2.right) and isMirror(t1.right, t2.left)
+
+        return isMirror(root, root)
+
+
+class SolutionIter:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        dq = deque([root, root])
+
+        while dq:
+            t1 = dq.popleft()
+            t2 = dq.popleft()
+
+            if t1 is None and t2 is None:
+                continue
+            if t1 is None or t2 is None:
+                return False
+            if t1.val != t2.val:
+                return False
+            dq.append(t1.left)
+            dq.append(t2.right)
+            dq.append(t1.right)
+            dq.append(t2.left)
+
+        return True
+
+
 def check_solution(l: List[int], expect: bool) -> None:
     s = Solution()
+    s_recur = SolutionRecur()
+    s_iter = SolutionIter()
 
     assert s.isSymmetric(make_tree_node(l)) == expect
+    assert s_recur.isSymmetric(make_tree_node(l)) == expect
+    assert s_iter.isSymmetric(make_tree_node(l)) == expect
 
 
 if __name__ == "__main__":
