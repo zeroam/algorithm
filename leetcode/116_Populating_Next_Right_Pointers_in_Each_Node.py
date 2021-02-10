a93c54af9f8fd3d5ade90ac9c57109f2ed63f1f1
jayone <imdff0803@gmail.com>
2021-02-10 18:29:07 +0900

    leetcode - 116. Populating Next Right Pointers in Each Node

---
+++ leetcode/116_Populating_Next_Right_Pointers_in_Each_Node.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root: Node) -> Node:
+        if root is None:
+            return None
+
+        dq = deque([root])
+        while dq:
+            prev = None
+            for _ in range(len(dq)):
+                node = dq.popleft()
+                if node.left:
+                    dq.append(node.left)
+                if node.right:
+                    dq.append(node.right)
+                if prev:
+                    prev.next = node
+                prev = node
+
+        return root
