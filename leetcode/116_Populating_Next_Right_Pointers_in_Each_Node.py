c8ec9dcef0039f8563041dd853d7385d5fa89789
jayone <imdff0803@gmail.com>
2021-02-16 09:49:32 +0900

    leetcode - 116. Populating Next Right Pointers in Each Node - add solution using pointer

--- leetcode/116_Populating_Next_Right_Pointers_in_Each_Node.py
+++ leetcode/116_Populating_Next_Right_Pointers_in_Each_Node.py
@@ -29,3 +29,37 @@ class Solution:
                 prev = node
 
         return root
+
+
+class SolutionPointers:
+    def connect(self, root: 'Node') -> 'Node':
+        if root is None:
+            return None
+
+        # Start with the root node. There are no next pointers
+        # that need to be set up on the first level
+        leftmost = root
+
+        # Once we reach the final level, we are done
+        while leftmost.left:
+
+            # Iterate the "linked list" starting from the head
+            # node and using the next pointers, establish the
+            # corresponding links for the next level
+            head = leftmost
+            while head:
+
+                # CONNECTION 1
+                head.left.next = head.right
+
+                # CONNECTION 2
+                if head.next:
+                    head.right.next = head.next.left
+
+                # Progress along the list (nodes on the current level)
+                head = head.next
+
+            # Move onto the next level
+            leftmost = leftmost.left
+
+        return root
