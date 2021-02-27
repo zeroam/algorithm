7588ccb44096294ab39138467c2ac1e9b3d2c667
jayone <imdff0803@gmail.com>
2021-02-27 23:24:40 +0900

    leetcode - 236. Lowest Common Ancestor of a Binary Tree - add solutions

--- leetcode/236_Lowest_Common_Ancestor_of_a_Binary_Tree.py
+++ leetcode/236_Lowest_Common_Ancestor_of_a_Binary_Tree.py
@@ -36,3 +36,71 @@ class Solution:
 
         _, node = findTree(root, p, q)
         return node
+
+
+class SolutionRecursive:
+    def __init__(self):
+        # Variable to store LCA node.
+        self.ans = None
+
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def recurse_tree(current_node):
+            # If reached the end of a branch, return False
+            if not current_node:
+                return False
+
+            # Left Recursion
+            left = recurse_tree(current_node.left)
+
+            # Right Recursion
+            right = recurse_tree(current_node.right)
+
+            # If the current node is one of p or q
+            mid = current_node == p or current_node == q
+
+            # If any two of the three flags left, right or mid become True
+            if mid + left + right >= 2:
+                self.ans = current_node
+
+            # Return True if either of the three bool values is True
+            return mid or left or right
+
+        # Traverse the tree
+        recurse_tree(root)
+        return self.ans
+
+
+class SolutionIterative:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # Stack for tree traversal
+        stack = [root]
+
+        # Dictionary for parent pointers
+        parent = {root: None}
+
+        # Iterate until we find both the nodes p and q
+        while p not in parent or q not in parent:
+            node = stack.pop()
+
+            # While traversing the tree, keep saving the parent pointers
+            if node.left:
+                parent[node.left]= node
+                stack.append(node.left)
+
+            if node.right:
+                parent[node.right] = node
+                stack.append(node.right)
+
+        # Ancestors set() for node p
+        ancestors = set()
+
+        # Process all ancestors for node p using parent pointers
+        while p:
+            ancestors.add(p)
+            p = parent[p]
+
+        # The first ancestor of q whilch appears in
+        # p's ancestor set() is their lowest common ancestor
+        while q not in ancestors:
+            q = parent[q]
+        return q
