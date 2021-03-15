2847e5d28fb041aa7a56a876dd33618aa24cd93f
jayone <imdff0803@gmail.com>
2021-03-15 10:08:10 +0900

    leetcode - 2. Add Two Numbers - add solution

--- leetcode/2_Add_Two_Numbers.py
+++ leetcode/2_Add_Two_Numbers.py
@@ -29,6 +29,26 @@ class Solution:
         return head.next
 
 
+class Solution2:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        prehead = prev = ListNode(-1)
+
+        carry = 0
+        while l1 or l2 or carry:
+            l1_val = l1.val if l1 else 0
+            l2_val = l2.val if l2 else 0
+            val = (l1_val + l2_val + carry) % 10
+            carry = (l1_val + l2_val + carry) // 10
+
+            prev.next = ListNode(val)
+            prev = prev.next
+
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return prehead.next
+
+
 def make_list_node(l: list) -> ListNode:
     node = ListNode(val=l[0])
     cur_node = node
@@ -41,21 +61,22 @@ def make_list_node(l: list) -> ListNode:
 
 
 def check_solution(l1: list, l2: list, expect: list):
-    s = Solution()
+    solutions = [Solution(), Solution2()]
 
-    l1 = make_list_node(l1)
-    l2 = make_list_node(l2)
-    expect = make_list_node(expect)
+    for s in solutions:
+        node1 = make_list_node(l1)
+        node2 = make_list_node(l2)
+        expect_node = make_list_node(expect)
 
-    res = s.addTwoNumbers(l1, l2)
+        res = s.addTwoNumbers(node1, node2)
 
-    while not None in [expect, res]:
-        assert expect.val == res.val
-        expect = expect.next
-        res = res.next
+        while not None in [expect_node, res]:
+            assert expect_node.val == res.val
+            expect_node = expect_node.next
+            res = res.next
 
-    assert expect is None
-    assert res is None
+        assert expect_node is None
+        assert res is None
 
 
 if __name__ == "__main__":
