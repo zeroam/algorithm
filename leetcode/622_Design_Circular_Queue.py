0d023280049da88b14f167af3906cbec394bf4a9
jayone <imdff0803@gmail.com>
2021-04-30 09:55:48 +0900

    leetcode - 622. Design Circular Queue - add solutions

--- leetcode/622_Design_Circular_Queue.py
+++ leetcode/622_Design_Circular_Queue.py
@@ -61,3 +61,105 @@ class MyCircularQueue:
         if self.circular and self.tail + 1 == self.head:
             return True
         return False
+
+
+class MyCircularQueueArray:
+    def __init__(self, k: int):
+        self.queue = [None] * k
+        self.head = 0
+        self.count = 0
+        self.capacity = k
+
+    def enQueue(self, value: int) -> bool:
+        if self.capacity == self.count:
+            return False
+
+        self.queue[(self.head + self.count) % self.capacity] = value
+        self.count += 1
+        return True
+
+    def deQueue(self) -> bool:
+        if self.count == 0:
+            return False
+
+        self.head = (self.head + 1) % self.capacity
+        self.count -= 1
+        return True
+
+    def Front(self) -> int:
+        if self.count == 0:
+            return -1
+        return self.queue[self.head]
+
+
+    def Rear(self) -> int:
+        if self.count == 0:
+            return -1
+        return self.queue[(self.head + self.count - 1) % self.capacity]
+
+    def isEmpty(self) -> bool:
+        if self.count == 0:
+            return True
+        return False
+
+    def isFull(self) -> bool:
+        if self.count == self.capacity:
+            return True
+        return False
+
+
+class Node:
+    def __init__(self, value, nextNode=None):
+        self.value = value
+        self.next = nextNode
+
+class MyCircularQueueLinkedList:
+    def __init__(self, k: int):
+        self.capacity = k
+        self.head = None
+        self.tail = None
+        self.count = 0
+
+    def enQueue(self, value: int) -> bool:
+        if self.capacity == self.count:
+            return False
+
+        if self.count == 0:
+            self.head = Node(value)
+            self.tail = self.head
+        else:
+            newNode = Node(value)
+            self.tail.next = newNode
+            self.tail = newNode
+        self.count += 1
+
+        return True
+
+    def deQueue(self) -> bool:
+        if self.count == 0:
+            return False
+
+        self.head = self.head.next
+        self.count -= 1
+        return True
+
+    def Front(self) -> int:
+        if self.count == 0:
+            return -1
+        return self.head.value
+
+
+    def Rear(self) -> int:
+        if self.count == 0:
+            return -1
+        return self.tail.value
+
+    def isEmpty(self) -> bool:
+        if self.count == 0:
+            return True
+        return False
+
+    def isFull(self) -> bool:
+        if self.count == self.capacity:
+            return True
+        return False
