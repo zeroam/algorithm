85889b11eea0db4020397754219b608635747adb
jayone <imdff0803@gmail.com>
2021-05-25 10:04:25 +0900

    leetcode - 232. Implement Queue using Stacks - add solutions

--- leetcode/232_Implement_Queue_using_Stacks.py
+++ leetcode/232_Implement_Queue_using_Stacks.py
@@ -37,6 +37,50 @@ class MyQueue:
         return not self.push_stack and not self.pop_stack
 
 
+class MyQueueS1:
+    def __init__(self):
+        self.front = None
+        self.push_stack = []
+        self.pop_stack = []
+
+    def push(self, x: int) -> None:
+        while self.pop_stack:
+            self.push_stack.append(self.pop_stack.pop())
+        self.push_stack.append(x)
+        while self.push_stack:
+            self.pop_stack.append(self.push_stack.pop())
+
+    def pop(self) -> int:
+        return self.pop_stack.pop()
+
+    def peek(self) -> int:
+        return self.pop_stack[-1]
+
+    def empty(self) -> bool:
+        return not self.pop_stack
+
+
+class MyQueueS2:
+    def __init__(self):
+        self.push_stack = []
+        self.pop_stack = []
+
+    def push(self, x: int) -> None:
+        self.push_stack.append(x)
+
+    def pop(self) -> int:
+        self.peek()
+        return self.pop_stack.pop()
+
+    def peek(self) -> int:
+        if not self.pop_stack:
+            while self.push_stack:
+                self.pop_stack.append(self.push_stack.pop())
+        return self.pop_stack[-1]
+
+    def empty(self) -> bool:
+        return not self.push_stack and not self.pop_stack
+
 
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
