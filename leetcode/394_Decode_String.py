b1efc16943f7b0d82c0fd61624cf0cfb2ec627f1
jayone <imdff0803@gmail.com>
2021-06-04 13:54:15 +0900

    leetcode - 394. Decode String - add solution for 2 stacks, recursive

--- leetcode/394_Decode_String.py
+++ leetcode/394_Decode_String.py
@@ -24,13 +24,65 @@ class Solution:
         return "".join(stack)
 
 
+class Solution2Stacks:
+    def decodeString(self, s: str = "") -> str:
+        cnt_stack = []
+        str_stack = []
+        cur_str = ""
+        k = 0
+        for ch in s:
+            if ch.isdigit():
+                k = 10 * k + int(ch)
+            elif ch == "[":
+                cnt_stack.append(k)
+                str_stack.append(cur_str)
+                k = 0
+                cur_str = ""
+            elif ch == "]":
+                decoded_str = str_stack.pop()
+                for _ in range(cnt_stack.pop()):
+                    decoded_str += cur_str
+                cur_str = decoded_str
+            else:
+                cur_str += ch
+
+        return cur_str
+
+
+class SolutionRecursive:
+    def __init__(self):
+        self.index = 0
+
+    def decodeString(self, s: str = "") -> str:
+        result = ""
+        while self.index < len(s) and s[self.index] != "]":
+            if s[self.index].isalpha():
+                result += s[self.index]
+                self.index += 1
+            elif s[self.index].isdigit():
+                k = 0
+                while self.index < len(s) and s[self.index].isdigit():
+                    k = k * 10 + int(s[self.index])
+                    self.index += 1
+                self.index += 1  # ignore [
+                decoded_string = self.decodeString(s)
+                self.index += 1  # ignore ]
+
+                for _ in range(k):
+                    result += decoded_string
+        return result
+
+
 def check_solutions(string: str, expect: str) -> None:
     s = Solution()
+    s_2stacks = Solution2Stacks()
 
     assert s.decodeString(string) == expect
+    assert s_2stacks.decodeString(string) == expect
 
 
 if __name__ == "__main__":
     check_solutions("3[a]2[bc]", "aaabcbc")
     check_solutions("10[abc]", "abcabcabcabcabcabcabcabcabcabc")
     check_solutions("3[a2[bc]]", "abcbcabcbcabcbc")
+    check_solutions("3[a2[b2[c]d]]", "abccdbccdabccdbccdabccdbccd")
