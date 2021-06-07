aeacc9028733e433fd99a2b47f4b20f29e567969
jayone <imdff0803@gmail.com>
2021-06-07 09:44:29 +0900

    leetcode - 733. Flood Fill - add solution for dfs

--- leetcode/733_Flood_Fill.py
+++ leetcode/733_Flood_Fill.py
@@ -29,12 +29,38 @@ class Solution:
         return image
 
 
+class SolutionDFS:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        m, n = len(image), len(image[0])
+        color = image[sr][sc]
+        if color == newColor:
+            return image
+
+        def dfs(r, c):
+            if image[r][c] == color:
+                image[r][c] = newColor
+                if r >= 1:
+                    dfs(r - 1, c)
+                if r + 1 < m:
+                    dfs(r + 1, c)
+                if c >= 1:
+                    dfs(r, c - 1)
+                if c + 1 < n:
+                    dfs(r, c + 1)
+
+        dfs(sr, sc)
+        return image
+
+
 def check_solutions(image: List[List[int]], sr: int, sc: int, new_color: int, expect: List[List[int]]):
     s = Solution()
+    s_dfs = SolutionDFS()
 
     assert s.floodFill(image, sr, sc, new_color) == expect
+    assert s_dfs.floodFill(image, sr, sc, new_color) == expect
 
 
 if __name__ == "__main__":
     check_solutions([[1, 1, 1], [1, 1, 0], [1, 0, 1]], 1, 1, 2, [[2, 2, 2], [2, 2, 0], [2, 0, 1]])
     check_solutions([[0, 0, 0], [0, 0, 0]], 0, 0, 2, [[2, 2, 2], [2, 2, 2]])
+    check_solutions([[0, 0, 0], [0, 1, 1]], 1, 1, 1, [[0, 0, 0], [0, 1, 1]])
