d58d0107743b15ea421ebb23a9accd8b5ac53835
jayone <imdff0803@gmail.com>
2021-06-25 08:41:21 +0900

    leetcode - 173. Binary Search Tree Iterator - add solution for controlled recursion

--- leetcode/173_Binary_Search_Tree_Iterator.py
+++ leetcode/173_Binary_Search_Tree_Iterator.py
@@ -64,6 +64,29 @@ class BSTIteratorDFS:
         return False
 
 
+class BSTIteratorControlledRecursion:
+    def __init__(self, root: TreeNode):
+        self.stack = []
+
+        self._leftmost_inorder(root)
+
+
+    def _leftmost_inorder(self, node):
+        while node:
+            self.stack.append(node)
+            node = node.left
+
+    def next(self) -> int:
+        topmost_node = self.stack.pop()
+
+        if topmost_node.right:
+            self._leftmost_inorder(topmost_node.right)
+        return topmost_node.val
+
+    def hasNext(self) -> bool:
+        return len(self.stack) > 0
+
+
 # Your BSTIterator object will be instantiated and called as such:
 # obj = BSTIterator(root)
 # param_1 = obj.next()
