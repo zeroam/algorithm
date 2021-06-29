e9c73270b9ab00632f3c14d9c37efef0a0d54ca9
jayone <imdff0803@gmail.com>
2021-06-29 09:37:13 +0900

    leetcode - 450. Delete Node in a BST - add solution

--- leetcode/450_Delete_Node_in_a_BST.py
+++ leetcode/450_Delete_Node_in_a_BST.py
@@ -26,14 +26,48 @@ class Solution:
                 temp = root.right
                 root = None
                 return temp
-            
+
             if root.right is None:
                 temp = root.left
                 root = None
                 return temp
-            
+
             min_node = self.findMinNode(root.right)
             root.val = min_node.val
-            root.right = self.deleteNode(root.right, min_node.val)    
-        
+            root.right = self.deleteNode(root.right, min_node.val)
+
+        return root
+
+
+class Solution:
+    def predecessor(self, node: TreeNode) -> int:
+        node = node.left
+        while node.right:
+            node = node.right
+        return node.val
+
+    def successor(self, node: TreeNode) -> int:
+        node = node.right
+        while node.left:
+            node = node.left
+        return node.val
+
+    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        if root is None:
+            return None
+
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if root.left is None and root.right is None:
+                root = None
+            elif root.left:
+                root.val = self.predecessor(root)
+                root.left = self.deleteNode(root.left, root.val)
+            else:
+                root.val = self.successor(root)
+                root.right = self.deleteNode(root.right, root.val)
+
         return root
