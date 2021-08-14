9b3d3667cad24455d3de4c39e9e5184c27e4f171
jayone <imdff0803@gmail.com>
2021-08-14 23:50:51 +0900

    leetcode - 349. Intersection of Two Arrays

---
+++ leetcode/349_Intersection_of_Two_Arrays.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1_set = set(nums1)
+        nums2_set = set(nums2)
+
+        return nums1_set & nums2_set
+
+
+def test_solution():
+    s = Solution()
+
+    assert s.intersection([1, 2, 2, 1], [2, 2]) == set([2])
