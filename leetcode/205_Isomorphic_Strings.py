4c57c5878b2ed69963d0b45697fb7351ac51acfe
jayone <imdff0803@gmail.com>
2021-08-17 11:14:19 +0900

    leetcode - 205. Isomorphic Strings - add solution

--- leetcode/205_Isomorphic_Strings.py
+++ leetcode/205_Isomorphic_Strings.py
@@ -17,13 +17,43 @@ class Solution:
         return True
 
 
+class SolutionIndexMapping:
+    def mapping_index(self, s: str) -> str:
+        mapper = {}
+        new_str = []
+
+        for i, c in enumerate(s):
+            if c not in mapper:
+                mapper[c] = i
+            new_str.append(str(mapper[c]))
+
+        return " ".join(new_str)
+
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        return self.mapping_index(s) == self.mapping_index(t)
+
+
 def check_cases(s: Solution):
+    """
+    requirements
+    1. We can map a character only to itself or to one other character.
+    2. No two character should map to same character
+    3. Replacing each character in string 's' with the character it is mapped to results in string 't'
+    """
 
     assert s.isIsomorphic("egg", "add") == True
     assert s.isIsomorphic("foo", "bar") == False
     assert s.isIsomorphic("paper", "title") == True
     assert s.isIsomorphic("badc", "baba") == False
+    assert (
+        s.isIsomorphic("abcdefghijklmnopqrstuvwxyzva", "abcdefghijklmnopqrstuvwxyzck")
+        == False
+    )
 
 
 def test_solution():
     check_cases(Solution())
+
+
+def test_solution_index_mapping():
+    check_cases(SolutionIndexMapping())
