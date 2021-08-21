622dc47e9b9907a5ad4f3fec59735eaa1f3a8466
jayone <imdff0803@gmail.com>
2021-08-21 16:57:52 +0900

    leetcode - 219. Contains Duplicate II

--- leetcode/219_Contains_Duplicate_II.py
+++ leetcode/219_Contains_Duplicate_II.py
@@ -17,6 +17,20 @@ class Solution:
         return False
 
 
+class SolutionHashTable:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        hash_set = set()
+        for i, num in enumerate(nums):
+            if num in hash_set:
+                return True
+
+            hash_set.add(num)
+            if len(hash_set) > k:
+                hash_set.remove(nums[i - k])
+
+        return False
+
+
 def check_cases(s: Solution):
     s.containsNearbyDuplicate([1, 2, 3, 1], 3) == True
     s.containsNearbyDuplicate([1, 0, 1, 1], 1) == True
@@ -26,3 +40,7 @@ def check_cases(s: Solution):
 
 def test_solution():
     check_cases(Solution())
+
+
+def test_solution_hash_table():
+    check_cases(SolutionHashTable())
