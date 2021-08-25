585bd04c4400eb74061dede18fb38033a2d9103e
jayone <imdff0803@gmail.com>
2021-08-25 09:10:50 +0900

    leetcode - 49. Group Anagram - add solution using sorted string, count characters

--- leetcode/49_Group_Anagrams.py
+++ leetcode/49_Group_Anagrams.py
@@ -12,6 +12,26 @@ class Solution:
         return list(hash_map.values())
 
 
+class SolutionSortedString:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash_map = defaultdict(list)
+        for word in strs:
+            hash_map[tuple(sorted(word))].append(word)
+        return hash_map.values()
+
+
+class SolutionCount:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash_map = defaultdict(list)
+        for word in strs:
+            count = [0] * 26
+            for c in word:
+                count[ord(c) - ord('c')] += 1
+            hash_map[tuple(count)].append(word)
+        return hash_map.values()
+
+
+
 def check_case(s: Solution, strs: List[str], expect: List[List[str]]):
     result = sorted([sorted(item) for item in s.groupAnagrams(strs)])
     expect = sorted([sorted(item) for item in expect])
@@ -33,3 +53,11 @@ def check_cases(s: Solution):
 
 def test_solution():
     check_cases(Solution())
+
+
+def test_solution_sorted_string():
+    check_cases(SolutionSortedString())
+
+
+def test_solution_count():
+    check_cases(SolutionCount())
