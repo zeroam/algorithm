9658f175073b15107f01cc932ab283e0422b8d7c
jayone <imdff0803@gmail.com>
2021-10-17 14:28:06 +0900

    leetcode - 547. Number of Province
    - add solution for using union find data structure

--- leetcode/547_Number_of_Province.py
+++ leetcode/547_Number_of_Province.py
@@ -97,6 +97,47 @@ class SolutionUnion:
         return count
 
 
+class UnionFind:
+    def __init__(self, n: int):
+        self.root = [i for i in range(n)]
+        self.rank = [1 for _ in range(n)]
+        self.count = n
+
+    def find(self, x: int) -> int:
+        if self.root[x] == x:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    def union(self, x: int, y: int) -> None:
+        rootx = self.find(x)
+        rooty = self.find(y)
+        if rootx != rooty:
+            if self.rank[rootx] > self.rank[rooty]:
+                self.root[rooty] = rootx
+            elif self.rank[rootx] < self.rank[rooty]:
+                self.root[rootx] = rooty
+            else:
+                self.root[rooty] = rootx
+                self.rank[rootx] += 1
+            self.count -= 1
+
+    def get_count(self):
+        return self.count
+
+class SolutionUnionFind:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        uf = UnionFind(n)
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                if isConnected[i][j] == 1:
+                    uf.union(i, j)
+
+        return uf.get_count()
+
+
 def check_cases(s: Solution):
     assert s.findCircleNum(
         [
@@ -135,3 +176,7 @@ def test_solution_bfs():
 
 def test_solution_union():
     check_cases(SolutionUnion())
+
+
+def test_solution_union_find():
+    check_cases(SolutionUnionFind())
