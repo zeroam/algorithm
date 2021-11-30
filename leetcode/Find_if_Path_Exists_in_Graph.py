a16f60d0b501bf14e3a4457de4b98f86342de58e
jayone <imdff0803@gmail.com>
2021-11-30 13:07:24 +0900

    leetcode - Find if Path Exists in Graph
    - add solutions for dfs, bfs, union find

--- leetcode/Find_if_Path_Exists_in_Graph.py
+++ leetcode/Find_if_Path_Exists_in_Graph.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from collections import defaultdict
+from collections import defaultdict, deque
 from typing import List
 
 
@@ -40,6 +40,88 @@ class Solution:
         return self.traverse_graph(start, end, visited, graph)
 
 
+class SolutionDFS:
+    def validPath(self, n: int, edges: List[List[int]], start: int, end: int) -> bool:
+        # make graph
+        graph = defaultdict(set)
+        for node_1, node_2 in edges:
+            graph[node_1].add(node_2)
+            graph[node_2].add(node_1)
+
+        # traverse
+        stack = [start]
+        visited = set()
+        while stack:
+            cur_node = stack.pop()
+            if cur_node == end:
+                return True
+
+            visited.add(cur_node)
+            neighbors = graph[cur_node]
+            for next_node in neighbors:
+                if next_node in visited:
+                    continue
+                stack.append(next_node)
+
+        return False
+
+
+class SolutionBFS:
+    def validPath(self, n: int, edges: List[List[int]], start: int, end: int) -> bool:
+        # make graph
+        graph = defaultdict(set)
+        for node_1, node_2 in edges:
+            graph[node_1].add(node_2)
+            graph[node_2].add(node_1)
+
+        # traverse
+        queue = deque([start])
+        visited = set()
+        while queue:
+            cur_node = queue.popleft()
+            if cur_node == end:
+                return True
+
+            visited.add(cur_node)
+            neighbors = graph[cur_node]
+            for next_node in neighbors:
+                if next_node in visited:
+                    continue
+                queue.append(next_node)
+
+        return False
+
+
+class UnionFind:
+    def __init__(self, n: int):
+        self.root = [i for i in range(n)]
+
+    def find(self, x: int):
+        if self.root[x] != x:
+            self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    def union(self, x: int, y: int):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x != root_y:
+            self.root[root_y] = root_x
+
+    def is_connected(self, x: int, y: int):
+        return self.find(x) == self.find(y)
+
+
+class SolutionUnionFind:
+    def validPath(self, n: int, edges: List[List[int]], start: int, end: int) -> bool:
+        uf = UnionFind(n)
+        for node_1, node_2 in edges:
+            uf.union(node_1, node_2)
+
+        return uf.is_connected(start, end)
+
+
+
+
 def check_cases(s: SolutionInterface):
     assert s.validPath(1, [], 0, 0) == True
     assert s.validPath(3, [[0, 1], [1, 2], [2, 0], [0, 2]], 0, 2) == True
@@ -49,3 +131,15 @@ def check_cases(s: SolutionInterface):
 
 def test_solution():
     check_cases(Solution())
+
+
+def test_solution_dfs():
+    check_cases(SolutionDFS())
+
+
+def test_solution_bfs():
+    check_cases(SolutionBFS())
+
+
+def test_solution_union_find():
+    check_cases(SolutionUnionFind())
