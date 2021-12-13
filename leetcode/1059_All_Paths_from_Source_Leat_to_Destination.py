dcca761a588550510f291e9b12bbaf9130b263e6
jayone <imdff0803@gmail.com>
2021-12-13 12:35:01 +0900

    leetcode - 1059. All Paths from Source Lead to Destination
    - solve problem

--- leetcode/1059_All_Paths_from_Source_Leat_to_Destination.py
+++ leetcode/1059_All_Paths_from_Source_Leat_to_Destination.py
@@ -7,8 +7,6 @@ class Solution:
         # make graph
         self.graph = graph = defaultdict(list)
         for a, b in edges:
-            if a == b:
-                return False
             graph[a].append(b)
 
         # make visited
@@ -46,6 +44,7 @@ def check_cases(s: Solution):
     assert s.leadsToDestination(n=3, edges=[[0, 1], [1, 1], [1, 2]], source=0, destination=2) == False
     assert s.leadsToDestination(n=2, edges=[[0, 1], [1, 1]], source=0, destination=1) == False
     assert s.leadsToDestination(n=5, edges=[[0, 1], [1, 2], [2, 3], [3, 4]], source=0, destination=3) == False
+    assert s.leadsToDestination(n=5, edges=[[0, 1], [1, 2], [2, 4], [3, 3], [3, 4]], source=0, destination=4) == True
 
 
 def test_solution():
