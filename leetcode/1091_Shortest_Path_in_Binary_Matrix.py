346d8a4c76d955c69ee54e70b21a1b080d350ef5
jayone <imdff0803@gmail.com>
2021-12-17 20:46:40 +0900

    leetcode - 1091. Shortest Path in Binary Matrix
    - add solution using visited

--- leetcode/1091_Shortest_Path_in_Binary_Matrix.py
+++ leetcode/1091_Shortest_Path_in_Binary_Matrix.py
@@ -32,6 +32,36 @@ class Solution:
         return -1
 
 
+class SolutionVisited:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        if grid[0][0] == 1 or grid[-1][-1] == 1:
+            return -1
+
+        n = len(grid)
+        directions = [(1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)]
+
+        queue = deque([(0, 0, 1)])
+        visited = set((0, 0))
+
+        while queue:
+            x, y, distance = queue.popleft()
+            if x == n - 1 and y == n - 1:
+                return distance
+
+            for dir_x, dir_y in directions:
+                next_x, next_y = x + dir_x, y + dir_y
+                if not 0 <= next_x < n or not 0 <= next_y < n:
+                    continue
+
+                if grid[next_x][next_y] == 1 or (next_x, next_y) in visited:
+                    continue
+
+                visited.add((next_x, next_y))
+                queue.append((next_x, next_y, distance + 1))
+
+        return -1
+
+
 def check_cases(s: Solution):
     s.shortestPathBinaryMatrix([[0, 1], [1, 0]]) == 2
     s.shortestPathBinaryMatrix([[0, 0, 0], [1, 1, 0], [1, 1, 0]]) == 4
@@ -41,3 +71,7 @@ def check_cases(s: Solution):
 
 def test_solution():
     check_cases(Solution())
+
+
+def test_solution_visited():
+    check_cases(SolutionVisited())
