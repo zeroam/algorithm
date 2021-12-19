2a8dc4c07bf41525d9fca6ddf25cc5d47d0a95a0
jayone <imdff0803@gmail.com>
2021-12-19 12:14:48 +0900

    leetcode - 429. N-ary Tree Level Order Traversal
    - add solutions for bfs, recursion

--- leetcode/429_N_ary_Tree_Level_Order_Traversal.py
+++ leetcode/429_N_ary_Tree_Level_Order_Traversal.py
@@ -32,3 +32,42 @@ class Solution:
             output.append(nodes)
 
         return output
+
+
+class SolutionSimpleBFS:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if root is None:
+            return []
+
+        output = []
+        previous_layer = [root]
+        while previous_layer:
+            current_layer = []
+            level = []
+
+            for node in previous_layer:
+                level.append(node.val)
+                current_layer.extend(node.children)
+            output.append(level)
+
+            previous_layer = current_layer
+
+        return output
+
+
+class SolutionRecursion:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        def traverse_node(node, level):
+            if len(output) == level:
+                output.append([])
+
+            output[level].append(node.val)
+            for child in node.children:
+                traverse_node(child, level + 1)
+
+        if root is None:
+            return []
+
+        output = []
+        traverse_node(root, 0)
+        return output
