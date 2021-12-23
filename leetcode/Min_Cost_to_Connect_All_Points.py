9e6baae1429fb5ea3af39c22d7b4cc959db542d1
jayone <imdff0803@gmail.com>
2021-12-23 14:29:00 +0900

    leetcode - Min Cost to Connect All Points
    - add solution Kruskal's algorithm

--- leetcode/Min_Cost_to_Connect_All_Points.py
+++ leetcode/Min_Cost_to_Connect_All_Points.py
@@ -1,3 +1,4 @@
+import heapq
 from typing import List
 
 
@@ -31,7 +32,7 @@ class Solution:
             for j in range(i + 1, size):
                 point1 = points[i]
                 point2 = points[j]
-                distance = abs(abs(point1[0] - point2[0]) + abs(point1[1] - point2[1]))
+                distance = abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
 
                 distances.append((i, j, distance))
 
@@ -57,9 +58,57 @@ class Solution:
         return value
 
 
+class SolutionKruskal:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        if not points or len(points) == 0:
+            return 0
+
+        size = len(points)
+        pq = []
+        uf = UnionFind(size)
+
+        for i in range(size):
+            x1, y1 = points[i]
+            for j in range(i + 1, size):
+                x2, y2 = points[j]
+                cost = abs(x1 - x2) + abs(y1 - y2)
+                edge = Edge(i, j, cost)
+                pq.append(edge)
+
+        # convert pq into a heap
+        heapq.heapify(pq)
+
+        result = 0
+        count = size - 1
+        while pq and count > 0:
+            edge = heapq.heappop(pq)
+            if not uf.is_connected(edge.point1, edge.point2):
+                uf.union(edge.point1, edge.point2)
+                result += edge.cost
+                count -= 1
+        return result
+
+
+class Edge:
+    def __init__(self, point1, point2, cost):
+        self.point1 = point1
+        self.point2 = point2
+        self.cost = cost
+
+    def __lt__(self, other):
+        return self.cost < other.cost
+
+    def __repr__(self):
+        return f"<Edge ({self.point1}, {self.point2}) ({self.cost})>"
+
+
 def check_cases(s: Solution):
     assert s.minCostConnectPoints([[3, 12], [-2, 5], [-4, 1]]) == 18
 
 
 def test_solution():
     check_cases(Solution())
+
+
+def test_solution_kruskal():
+    check_cases(SolutionKruskal())
