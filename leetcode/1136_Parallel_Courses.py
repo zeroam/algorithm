5a333e3380cac6ec3022ad24772fe3db506d8c35
jayone <imdff0803@gmail.com>
2022-02-04 13:19:34 +0900

    leetcode - 1136. Parallel Courses
    - add dfs solution

--- leetcode/1136_Parallel_Courses.py
+++ leetcode/1136_Parallel_Courses.py
@@ -29,6 +29,82 @@ class Solution:
         return semester
 
 
+class SolutionDFS:
+    def minimumSemesters(self, n: int, relations: List[List[int]]) -> int:
+        adj_list = [[] for _ in range(n)]
+        for src, dest in relations:
+            adj_list[src - 1].append(dest - 1)
+
+        visited = {}
+        def dfs(node):
+            if node in visited:
+                return visited[node]
+
+            visited[node] = -1
+            max_length = 1
+            for nei in adj_list[node]:
+                length = dfs(nei)
+                if length == -1:
+                    return -1
+                max_length = max(length + 1, max_length)
+
+            visited[node] = max_length
+            return max_length
+
+        max_length = 1
+        for node in range(n):
+            length = dfs(node)
+            if length == -1:
+                return -1
+            max_length = max(max_length, length)
+
+        return max_length
+
+
+class SolutionDFS2:
+    def minimumSemesters(self, n: int, relations: List[List[int]]) -> int:
+        adj_list = [[] for _ in range(n)]
+        for src, dest in relations:
+            adj_list[src - 1].append(dest - 1)
+
+        visited = {}
+        def check_cycle(node):
+            if node in visited:
+                return visited[node]
+
+            visited[node] = True
+            for nei in adj_list[node]:
+                if check_cycle(nei):
+                    return True
+
+            visited[node] = False
+            return False
+
+        for node in range(n):
+            if check_cycle(node):
+                return -1
+
+        visited_length = {}
+        def dfs(node):
+            if node in visited_length:
+                return visited_length[node]
+
+            max_length = 1
+            for nei in adj_list[node]:
+                length = dfs(nei)
+                max_length = max(length + 1, max_length)
+
+            visited_length[node] = max_length
+            return max_length
+
+        max_length = 1
+        for node in range(n):
+            length = dfs(node)
+            max_length = max(max_length, length)
+
+        return max_length
+
+
 def check_cases(s: Solution):
     assert s.minimumSemesters(3, [[1, 3], [2, 3]]) == 2
     assert s.minimumSemesters(3, [[1, 2], [2, 3], [3, 1]]) == -1
@@ -36,3 +112,11 @@ def check_cases(s: Solution):
 
 def test_solution():
     check_cases(Solution())
+
+
+def test_solution_dfs():
+    check_cases(SolutionDFS())
+
+
+def test_solution_dfs2():
+    check_cases(SolutionDFS2())
