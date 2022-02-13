ea6fc75119da847ea8278609fd5d117f565443c5
jayone <imdff0803@gmail.com>
2022-02-13 15:39:45 +0900

    leetcode - 215. Kth Largest Element in an Array

--- leetcode/215_Kth_Largest_Element_in_an_Array.py
+++ leetcode/215_Kth_Largest_Element_in_an_Array.py
@@ -1,4 +1,5 @@
 import heapq
+import random
 from typing import List
 
 
@@ -18,6 +19,39 @@ class Solution:
         return min_heap[0]
 
 
+class SolutionQuickSelect:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        def partition(left, right, pivot_index):
+            pivot = nums[pivot_index]
+
+            nums[pivot_index], nums[right] = nums[right], nums[pivot_index]
+
+            store_index = left
+            for i in range(left, right):
+                if nums[i] < pivot:
+                    nums[store_index], nums[i] = nums[i], nums[store_index]
+                    store_index += 1
+
+            nums[right], nums[store_index] = nums[store_index], nums[right]
+            return store_index
+
+        def select(left, right, kth_smallest):
+            if left == right:
+                return nums[left]
+
+            pivot_index = random.randint(left, right)
+            pivot_index = partition(left, right, pivot_index)
+
+            if kth_smallest == pivot_index:
+                return nums[kth_smallest]
+            elif kth_smallest < pivot_index:
+                return select(left, pivot_index - 1, kth_smallest)
+            else:
+                return select(pivot_index + 1, right, kth_smallest)
+
+        return select(0, len(nums) - 1, len(nums) - k)
+
+
 def check_cases(s: Solution):
     assert s.findKthLargest([3, 2, 1, 5, 6, 4], 2) == 5
     assert s.findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4) == 4
@@ -26,3 +60,7 @@ def check_cases(s: Solution):
 
 def test_solution():
     check_cases(Solution())
+
+
+def test_solution_quickselect():
+    check_cases(SolutionQuickSelect())
