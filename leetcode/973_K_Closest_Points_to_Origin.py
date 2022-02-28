c6cf3e3059e3cb7deb4b840c9408a1d05d5bee1d
jayone <imdff0803@gmail.com>
2022-02-28 13:19:54 +0900

    leetcode - 973. K Closest Points to Origin
    - add solution

--- leetcode/973_K_Closest_Points_to_Origin.py
+++ leetcode/973_K_Closest_Points_to_Origin.py
@@ -17,6 +17,19 @@ class Solution:
         return ans
 
 
+class SolutionCustomComparator:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        points.sort(key=lambda x: x[0] ** 2 + x[1] ** 2)
+        return points[:k]
+
+
+class SolutionMinHeap:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        min_heap = [(point[0] ** 2 + point[1] ** 2, point) for point in points]
+        heapq.heapify(min_heap)
+        return [heapq.heappop(min_heap)[1] for _ in range(k)]
+
+
 def check_cases(s: Solution):
     s.kClosest([[1, 3], [-2, 2]], 1) == [[-2, 2]]
     s.kClosest([[3, 3], [5, -1], [-2, 4]], 2) == [[3, 3], [-2, 4]]
@@ -24,3 +37,11 @@ def check_cases(s: Solution):
 
 def test_solution():
     check_cases(Solution())
+
+
+def test_solution_custom_comparator():
+    check_cases(SolutionCustomComparator())
+
+
+def test_solution_min_heap():
+    check_cases(SolutionMinHeap())
