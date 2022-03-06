788b7b635539101c98e205c76717af0fde959cc3
jayone <imdff0803@gmail.com>
2022-03-06 21:35:27 +0900

    leetcode - 1642. Furthest Building You Can Reach
    - add solution using min-heap, max-heap

--- leetcode/1642_Furthest_Building_You_Can_Reach.py
+++ leetcode/1642_Furthest_Building_You_Can_Reach.py
@@ -35,12 +35,61 @@ class Solution:
         return i + int(complete)
 
 
+class SolutionMinHeap:
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        ladders_allocation = []
+        for i in range(len(heights) - 1):
+            diff = heights[i + 1] - heights[i]
+            if diff <= 0:  # down or equal
+                continue
+
+            heapq.heappush(ladders_allocation, diff)
+            if len(ladders_allocation) <= ladders:
+                continue
+
+            bricks -= heapq.heappop(ladders_allocation)
+            if bricks < 0:
+                return i
+
+        return len(heights) - 1
+
+
+class SolutionMaxHeap:
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        bricks_allocation = []
+        for i in range(len(heights) - 1):
+            diff = heights[i + 1] - heights[i]
+            if diff <= 0:  # down or equal
+                continue
+
+            heapq.heappush(bricks_allocation, -diff)
+            bricks -= diff
+            if bricks >= 0:
+                continue
+            if ladders == 0:
+                return i
+
+            bricks += -heapq.heappop(bricks_allocation)
+            ladders -= 1
+
+        return len(heights) - 1
+
+
 def check_cases(s: Solution):
     assert s.furthestBuilding([14, 3, 19, 3], 17, 0) == 3
     assert s.furthestBuilding([4, 2, 7, 6, 9, 14, 12], 5, 1) == 4
     assert s.furthestBuilding([4, 12, 2, 7, 3, 18, 20, 3, 19], 10, 2) == 7
     assert s.furthestBuilding([14, 3, 19, 3], 17, 0) == 3
+    assert s.furthestBuilding([2, 7, 9, 12], 5, 1) == 3
 
 
 def test_solution():
     check_cases(Solution())
+
+
+def test_solution_min_heap():
+    check_cases(SolutionMinHeap())
+
+
+def test_solution_max_heap():
+    check_cases(SolutionMaxHeap())
