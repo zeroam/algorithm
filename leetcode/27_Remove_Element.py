3127c05d46fa1203c716dd82e2556a5e8ad4fbb1
jayone <imdff0803@gmail.com>
2022-03-23 22:48:48 +0900

    leetcode - 27. Remove Element
    - add solution two pointers

--- leetcode/27_Remove_Element.py
+++ leetcode/27_Remove_Element.py
@@ -30,16 +30,36 @@ class SolutionTwoPointers:
         return i
 
 
-def check_solution(nums: List[int], val: int, expect: List[int]):
-    solutions = [Solution(), SolutionTwoPointers()]
+class SolutionTwoPointers2:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
 
-    for s in solutions:
-        nums_copy = nums.copy()
-        size = s.removeElement(nums_copy, val)
-        assert nums_copy[:size] == expect
+        return k
 
 
-if __name__ == "__main__":
-    check_solution([1, 2, 2, 3, 4], 2, [1, 3, 4])
-    check_solution([1, 2, 3, 4], 2, [1, 3, 4])
-    check_solution([3, 4, 3, 1], 3, [4, 1])
+def check_solution(s: Solution, nums: List[int], val: int, expect: List[int]):
+    nums_copy = nums.copy()
+    size = s.removeElement(nums_copy, val)
+    assert nums_copy[:size] == expect
+
+
+def check_cases(s: Solution):
+    check_solution(s, [1, 2, 2, 3, 4], 2, [1, 3, 4])
+    check_solution(s, [1, 2, 3, 4], 2, [1, 3, 4])
+    check_solution(s, [3, 4, 3, 1], 3, [4, 1])
+
+
+def test_solution():
+    check_cases(Solution())
+
+
+def test_solution_two_pointers():
+    check_cases(SolutionTwoPointers())
+
+
+def test_solution_two_pointers2():
+    check_cases(SolutionTwoPointers2())
