15e5b321592619382c3dde962ef73cff0dcf1e4b
jayone <imdff0803@gmail.com>
2022-03-29 00:15:16 +0900

    leetcode - 209. Minimum Size Subarray Sum
    - add solution bruteforce, two pointers

--- leetcode/209_Minimum_Size_Subarray_Sum.py
+++ leetcode/209_Minimum_Size_Subarray_Sum.py
@@ -1,3 +1,4 @@
+import sys
 from typing import List
 
 
@@ -17,14 +18,72 @@ class Solution:
         return 0
 
 
+class Solution2:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        n = len(nums)
+        sums = [0] * n
+        for i in range(1, n):
+            sums[i] = sums[i - 1] + nums[i]
+
+        for size in range(0, n):
+            for i in range(size, n):
+                total = sums[i] - sums[i - size] + nums[i - size]
+                if total >= target:
+                    return size + 1
+        return 0
+
+
+class SolutionBruteForce:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        n = len(nums)
+        sums = [0] * n
+        for i in range(1, n):
+            sums[i] = sums[i - 1] + nums[i]
+
+        ans = sys.maxsize
+        for i in range(n):
+            for j in range(i, n):
+                total = sums[j] - sums[i] + nums[i]
+                if total >= target:
+                    ans = min(ans, j - i + 1)
+                    break
+        return ans if ans != sys.maxsize else 0
+
+
+class SolutionTwoPointers:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        n = len(nums)
+
+        ans = sys.maxsize
+        left = 0
+        total = 0
+        for i in range(n):
+            total += nums[i]
+            while total >= target:
+                ans = min(ans, i - left + 1)
+                total -= nums[left]
+                left += 1
+        return ans if ans != sys.maxsize else 0
 
 
 def check_cases(s: Solution):
     assert s.minSubArrayLen(11, [1, 2, 3, 4, 5]) == 3
-    assert s.minSubArrayLen(213, [12,28,83,4,25,26,25,2,25,25,25,12]) == 8
+    assert s.minSubArrayLen(213, [12, 28, 83, 4, 25, 26, 25, 2, 25, 25, 25, 12]) == 8
     assert s.minSubArrayLen(11, [1, 2, 3, 4, 5]) == 3
     assert s.minSubArrayLen(15, [1, 2, 3, 4, 5]) == 5
 
 
 def test_solution():
     check_cases(Solution())
+
+
+def test_solution2():
+    check_cases(Solution2())
+
+
+def test_solution_brute_force():
+    check_cases(SolutionBruteForce())
+
+
+def test_solution_two_pointers():
+    check_cases(SolutionTwoPointers())
