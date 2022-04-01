6eeb380b6b177b35ff4d9ba3cf6b4f219c197646
jayone <imdff0803@gmail.com>
2022-04-01 13:25:10 +0900

    leetcode - 151. Reverse Words in a String
    - add solution implement builtin methods

--- leetcode/151_Reverse_Words_in_a_String.py
+++ leetcode/151_Reverse_Words_in_a_String.py
@@ -28,6 +28,50 @@ class SolutionStack:
         return result.strip()
 
 
+class SolutionReverse:
+    def trim_spaces(self, s: str) -> list:
+        left, right = 0, len(s) - 1
+        # remove leading spaces
+        while left <= right and s[left] == " ":
+            left += 1
+
+        # remove trailing spaces
+        while left <= right and s[right] == " ":
+            right -= 1
+
+        output = []
+        while left <= right:
+            if s[left] != " ":
+                output.append(s[left])
+            elif output[-1] != " ":
+                output.append(" ")
+            left += 1
+
+        return output
+
+    def reverse_array(self, l: list, start: int, end: int):
+        while start < end:
+            l[start], l[end] = l[end], l[start]
+            start, end = start + 1, end - 1
+
+    def reverseWords(self, s: str) -> str:
+        output = self.trim_spaces(s)
+        self.reverse_array(output, 0, len(output) - 1)
+
+        n = len(output)
+        start, end = 0, 0
+        while start < n:
+            while end < n and output[end] != " ":
+                end += 1
+            self.reverse_array(output, start, end - 1)
+
+            # move to next word
+            start = end + 1
+            end += 1
+
+        return "".join(output)
+
+
 def check_cases(s: Solution):
     assert s.reverseWords("the sky is blue") == "blue is sky the"
     assert s.reverseWords("  hello world  ") == "world hello"
@@ -40,3 +84,7 @@ def test_solution():
 
 def test_solution_stack():
     check_cases(SolutionStack())
+
+
+def test_solution_reverse():
+    check_cases(SolutionReverse())
