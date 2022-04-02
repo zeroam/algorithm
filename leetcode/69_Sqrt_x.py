eee051940eced2b8b0d640c3239846d74893fac4
jayone <imdff0803@gmail.com>
2022-04-02 16:52:16 +0900

    leetcode - 69. Sqrt(X)
    - add solution builtin, binary search

--- leetcode/69_Sqrt_x.py
+++ leetcode/69_Sqrt_x.py
@@ -16,6 +16,30 @@ class Solution:
                 return mid
 
 
+class SolutionBuiltin:
+    def mySqrt(self, x: int) -> int:
+        return int(x ** 0.5)
+
+
+class SolutionBinarySearch:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+
+        left, right = 2, x // 2
+        while left <= right:
+            mid = (left + right) // 2
+            num = mid * mid
+            if num > x:
+                right = mid - 1
+            elif num < x:
+                left = mid + 1
+            else:
+                return mid
+
+        return right
+
+
 def check_cases(s: Solution):
     s.mySqrt(1) == 1
     s.mySqrt(4) == 2
@@ -26,3 +50,11 @@ def check_cases(s: Solution):
 
 def test_solution():
     check_cases(Solution())
+
+
+def test_solution_builtin():
+    check_cases(SolutionBuiltin())
+
+
+def test_solution_binary_search():
+    check_cases(SolutionBinarySearch())
