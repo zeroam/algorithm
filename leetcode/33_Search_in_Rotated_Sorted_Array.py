96c26410ffd4cd4ad6050ce975673a958e523b71
jayone <imdff0803@gmail.com>
2022-04-04 13:03:12 +0900

    leetcode - 33. Search in Rotated Sorted Array
    - add solution using rotated index

--- leetcode/33_Search_in_Rotated_Sorted_Array.py
+++ leetcode/33_Search_in_Rotated_Sorted_Array.py
@@ -22,6 +22,50 @@ class Solution:
         return -1
 
 
+class SolutionRotatedIndex:
+    def search(self, nums: List[int], target: int) -> int:
+        def find_rotated_index(left, right):
+            if nums[left] < nums[right]:
+                return 0
+
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] > nums[mid + 1]:
+                    return mid + 1
+
+                if nums[mid] < nums[left]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+        def binary_search(left, right):
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] < target:
+                    left = mid + 1
+                elif nums[mid] > target:
+                    right = mid - 1
+                else:
+                    return mid
+            return -1
+
+        n = len(nums)
+        if n == 1:
+            return 0 if nums[0] == target else -1
+
+        left, right = 0, n - 1
+        rotated_index = find_rotated_index(left, right)
+
+        if nums[rotated_index] == target:
+            return rotated_index
+        if rotated_index == 0:
+            return binary_search(0, len(nums) - 1)
+
+        if nums[0] > target:
+            return binary_search(rotated_index, len(nums) - 1)
+        return binary_search(0, rotated_index - 1)
+
+
 def check_cases(s: Solution):
     assert s.search([4, 5, 6, 7, 0, 1, 2], 0) == 4
     assert s.search([4, 5, 6, 7, 0, 1, 2], 3) == -1
@@ -30,3 +74,7 @@ def check_cases(s: Solution):
 
 def test_solutino():
     check_cases(Solution())
+
+
+def test_solution_rotated_index():
+    check_cases(SolutionRotatedIndex())
