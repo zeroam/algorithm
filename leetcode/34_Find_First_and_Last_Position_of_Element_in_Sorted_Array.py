6355de47beca0e1b99a749de23f1eb1b9f444220
jayone <imdff0803@gmail.com>
2022-04-23 20:17:42 +0900

    leetcode - 34. Find First and Last Position of Element in Sorted Array
    - add binary search solution

--- leetcode/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
+++ leetcode/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -16,6 +16,39 @@ class Solution:
         return min(result), max(result)
 
 
+class SolutionBinarySearch:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        lower_bound = self.find_bound(nums, target, True)
+        if lower_bound == -1:
+            return [-1, -1]
+
+        upper_bound = self.find_bound(nums, target, False)
+
+        return [lower_bound, upper_bound]
+
+    def find_bound(self, nums, target, is_first):
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                if is_first:
+                    if mid == left or nums[mid - 1] < target:
+                        return mid
+                    right = mid - 1
+                else:
+                    if mid == right or nums[mid + 1] > target:
+                        return mid
+                    left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return -1
+
+
 def check_cases(s: Solution):
     s.searchRange([5, 7, 7, 8, 8, 10], 8) == [3, 4]
     s.searchRange([5, 7, 7, 8, 8, 10], 6) == [-1, -1]
@@ -25,3 +58,7 @@ def check_cases(s: Solution):
 
 def test_solution():
     check_cases(Solution())
+
+
+def test_solution_binary_search():
+    check_cases(SolutionBinarySearch())
