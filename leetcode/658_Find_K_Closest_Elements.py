7625ee0420d8bcd222de2f145ad98149b1da9a30
jayone <imdff0803@gmail.com>
2022-04-25 13:48:24 +0900

    leetcod - 658. Find K Closest Element
    - add solution using sort, binary search

--- leetcode/658_Find_K_Closest_Elements.py
+++ leetcode/658_Find_K_Closest_Elements.py
@@ -44,6 +44,35 @@ class Solution:
         return right
 
 
+class SolutionSort:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        arr.sort(key=lambda num: abs(num - x))
+        return sorted(arr[:k])
+
+
+class SolutionBinarySearch:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        left, right = 0, len(arr) - 1
+        while left + 1 < right:
+            mid = (left + right) // 2
+            if arr[mid] >= x:
+                right = mid
+            else:
+                left = mid
+
+        while right - left - 1 < k:
+            if left == -1:
+                right += 1
+                continue
+
+            if right == len(arr) or abs(arr[left] - x) <= abs(arr[right] - x):
+                left -= 1
+            else:
+                right += 1
+
+        return [arr[i] for i in range(left + 1, right)]
+
+
 def check_cases(s: Solution):
     assert s.findClosestElements([1], 1, 1) == [1]
     assert s.findClosestElements([1, 2, 3, 4, 5], 4, 3) == [1, 2, 3, 4]
@@ -55,3 +84,11 @@ def check_cases(s: Solution):
 
 def test_solution():
     check_cases(Solution())
+
+
+def test_solution_sort():
+    check_cases(SolutionSort())
+
+
+def test_solution_binary_search():
+    check_cases(SolutionBinarySearch())
