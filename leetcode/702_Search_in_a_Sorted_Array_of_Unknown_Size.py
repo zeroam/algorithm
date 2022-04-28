b347f20cb18186064fee7d370ecb8d3f40adfea5
jayone <imdff0803@gmail.com>
2022-04-29 07:29:09 +0900

    leetcode - 702. Search in a Sorted Array of Unknown Size
    - add solution

--- leetcode/702_Search_in_a_Sorted_Array_of_Unknown_Size.py
+++ leetcode/702_Search_in_a_Sorted_Array_of_Unknown_Size.py
@@ -16,3 +16,23 @@ class Solution:
                 left = mid + 1
 
         return -1
+
+
+class Solution2:
+    def search(self, reader: 'ArrayReader', target: int) -> int:
+        # 1. define boundary
+        left, right = 0, 1
+        while reader.get(right) < target:
+            right <<= 1
+
+        # 2. binary search
+        while left <= right:
+            mid = (left + right) // 2
+            if reader.get(mid) == target:
+                return mid
+            if reader.get(mid) > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return -1
