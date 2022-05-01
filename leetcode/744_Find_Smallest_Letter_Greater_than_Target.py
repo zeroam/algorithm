38d47d5af0477eb487831a41833cbb6a39dfecbd
jayone <imdff0803@gmail.com>
2022-05-01 21:31:59 +0900

    leetcode - 744. Find Smallest Letter Greater than Target
    - add solutions

--- leetcode/744_Find_Smallest_Letter_Greater_than_Target.py
+++ leetcode/744_Find_Smallest_Letter_Greater_than_Target.py
@@ -1,3 +1,4 @@
+from bisect import bisect
 from typing import List
 
 
@@ -21,6 +22,41 @@ class Solution:
         return letters[left % len(letters)]
 
 
+class SolutionSeen:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        seen = set(letters)
+        for i in range(1, 26):
+            cand = chr((ord(target) - ord("a") + i) % 26 + ord("a"))
+            if cand in seen:
+                return cand
+
+
+class SolutionLinear:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        for char in letters:
+            if char > target:
+                return char
+        return letters[0]
+
+
+class SolutionBinarySearch:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        left, right = 0, len(letters) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if letters[mid] <= target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return letters[left % len(letters)]
+
+
+class SolutionBisect:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        index = bisect(letters, target)
+        return letters[index % len(letters)]
+
+
 def check_cases(s: Solution):
     assert s.nextGreatestLetter(["c", "f", "j"], "a") == "c"
     assert s.nextGreatestLetter(["c", "f", "j"], "c") == "f"
@@ -32,3 +68,19 @@ def check_cases(s: Solution):
 
 def test_solution():
     check_cases(Solution())
+
+
+def test_solution_seen():
+    check_cases(SolutionSeen())
+
+
+def test_solution_linear():
+    check_cases(SolutionLinear())
+
+
+def test_solution_binary_search():
+    check_cases(SolutionBinarySearch())
+
+
+def test_solution_bisect():
+    check_cases(SolutionBisect())
