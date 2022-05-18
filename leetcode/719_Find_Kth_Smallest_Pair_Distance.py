cd528c18a9985701eeb8249c414f584cd256d148
jayone <imdff0803@gmail.com>
2022-05-18 13:23:15 +0900

    leetcode - 719. Find Kth Smallest Pair Distance
    - add binary solution

--- leetcode/719_Find_Kth_Smallest_Pair_Distance.py
+++ leetcode/719_Find_Kth_Smallest_Pair_Distance.py
@@ -12,6 +12,30 @@ class Solution:
         return distances[k - 1]
 
 
+class SolutionBinary:
+    def smallestDistancePair(self, nums: List[int], k: int) -> int:
+        def possible(guess_dist):
+            i = count = 0
+            j = 1
+            while i < len(nums):
+                while j < len(nums) and nums[j] - nums[i] <= guess_dist:
+                    j += 1
+                count += j - i - 1
+                i += 1
+            return count >= k
+
+        nums.sort()
+        low, high = 0, nums[-1] - nums[0]
+        while low < high:
+            mid = (low + high) // 2
+            if possible(mid):
+                high = mid
+            else:
+                low = mid + 1
+
+        return low
+
+
 def check_cases(s: Solution):
     assert s.smallestDistancePair([1, 3, 1], 1) == 0
     assert s.smallestDistancePair([1, 1, 1], 2) == 0
@@ -20,3 +44,7 @@ def check_cases(s: Solution):
 
 def test_solution():
     check_cases(Solution())
+
+
+def test_solution_binary():
+    check_cases(SolutionBinary())
