8bc24767508acbcd1853fb337db8b77b909ff636
jayone <imdff0803@gmail.com>
2022-05-29 16:31:14 +0900

    leetcode - 410. Split Array Largest Sum
    - add solution dynamic programming

--- leetcode/410_Split_Array_Largest_Sum.py
+++ leetcode/410_Split_Array_Largest_Sum.py
@@ -1,3 +1,5 @@
+from functools import lru_cache
+import itertools
 import sys
 from typing import List
 
@@ -28,6 +30,29 @@ class Solution:
         return largest_sum
 
 
+class SolutionDynamic:
+    def splitArray(self, nums: List[int], m: int) -> int:
+        n = len(nums)
+        acc_nums = [0] + list(itertools.accumulate(nums))
+
+        @lru_cache(None)
+        def split_sums(curr_index: int, arr_cnt: int) -> int:
+            if arr_cnt == 1:
+                return acc_nums[n] - acc_nums[curr_index]
+
+            min_sum = acc_nums[n]
+            for i in range(curr_index, n - arr_cnt + 1):
+                first_sum = acc_nums[i + 1] - acc_nums[curr_index]
+                largest_sum = max(first_sum, split_sums(i + 1, arr_cnt - 1))
+                min_sum = min(min_sum, largest_sum)
+                if first_sum >= min_sum:
+                    break
+
+            return min_sum
+
+        return split_sums(0, m)
+
+
 def check_cases(s: Solution):
     s.splitArray([7, 2, 5, 10, 8], 2) == 18
     s.splitArray([1, 2, 3, 4, 5], 2) == 9
@@ -36,3 +61,7 @@ def check_cases(s: Solution):
 
 def test_solution():
     check_cases(Solution())
+
+
+def test_solution_dynamic():
+    check_cases(SolutionDynamic())
