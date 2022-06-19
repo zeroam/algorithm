f464308da97696195aedae70aac4241a693ba6fc
jayone <imdff0803@gmail.com>
2022-06-19 14:07:48 +0900

    leetcode - 234. Palindrome Linked List
    - add solution using list, deque, runner

--- leetcode/234_Palindrome_Linked_List.py
+++ leetcode/234_Palindrome_Linked_List.py
@@ -1,4 +1,5 @@
-from typing import List
+from typing import Optional
+from collections import deque
 from common.node import ListNode, listnode_to_list, list_to_listnode
 
 
@@ -38,16 +39,71 @@ class Solution2:
         return self.ans
 
 
-def check_solution(head: List[int], expect: bool):
-    s = Solution()
-    s2 = Solution2()
+class SolutionList:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        node = head
+        l = []
+        while node:
+            l.append(node.val)
+            node = node.next
 
-    assert s.isPalindrome(list_to_listnode(head)) == expect
-    assert s2.isPalindrome(list_to_listnode(head)) == expect
+        return l == l[::-1]
 
 
-if __name__ == "__main__":
-    check_solution([1], True)
-    check_solution([1, 2, 2, 1], True)
-    check_solution([1, 2], False)
-    check_solution([1, 2, 1], True)
+class SolutionDeque:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        node = head
+        queue = deque()
+        while node:
+            queue.append(node.val)
+            node = node.next
+
+        while len(queue) > 1:
+            if queue.popleft() != queue.pop():
+                return False
+        return True
+
+
+class SolutionRunner:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        fast = slow = head
+        rev = None
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
+        if fast:
+            slow = slow.next
+
+        while rev:
+            if slow.val != rev.val:
+                return False
+            slow, rev = slow.next, rev.next
+        return True
+
+
+def check_cases(s: Solution):
+    assert s.isPalindrome(None) == True
+    assert s.isPalindrome(list_to_listnode([1])) == True
+    assert s.isPalindrome(list_to_listnode([1, 2, 2, 1])) == True
+    assert s.isPalindrome(list_to_listnode([1, 2])) == False
+    assert s.isPalindrome(list_to_listnode([1, 2, 1])) == True
+
+
+def test_solution():
+    check_cases(Solution())
+
+
+def test_solution2():
+    check_cases(Solution2())
+
+
+def test_solution_list():
+    check_cases(SolutionList())
+
+
+def test_solution_deque():
+    check_cases(SolutionDeque())
+
+
+def test_solution_runner():
+    check_cases(SolutionRunner())
