d473385ecc3c4668ca6955bd6223ca60310a7f70
jayone <imdff0803@gmail.com>
2022-06-19 15:44:28 +0900

    leetcode - 206. Reverse Linked List
    - add solutions

--- leetcode/206_Reverse_Linked_List.py
+++ leetcode/206_Reverse_Linked_List.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Optional
 from common.node import ListNode, list_to_listnode, listnode_to_list
 
 
@@ -31,6 +31,15 @@ class SolutionIterative:
         return prev
 
 
+class SolutionIterative2:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        node, prev = head, None
+        while node:
+            next_node, node.next = node.next, prev
+            node, prev = next_node, node
+        return prev
+
+
 class SolutionRecursive:
     def reverseList(self, head: ListNode) -> ListNode:
         if head is None or head.next is None:
@@ -43,16 +52,38 @@ class SolutionRecursive:
         return p
 
 
-def check_solution(l: List[int], expect: List[int]) -> None:
-    s = Solution()
-    s_i = SolutionIterative()
-    s_r = SolutionRecursive()
+class SolutionRecursive2:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def reverse(node: Optional[ListNode], prev: Optional[ListNode] = None):
+            if not node:
+                return prev
+            next_node, node.next = node.next, prev
+            return reverse(next_node, node)
 
-    assert listnode_to_list(s.reverseList(list_to_listnode(l))) == expect
-    assert listnode_to_list(s_i.reverseList(list_to_listnode(l))) == expect
+        return reverse(head)
 
 
-if __name__ == "__main__":
-    check_solution([1, 2, 3, 4, 5], [5, 4, 3, 2, 1])
-    check_solution([1, 2], [2, 1])
-    check_solution([], [])
+def check_cases(s: Solution):
+    assert listnode_to_list(s.reverseList(list_to_listnode([1, 2, 3, 4, 5]))) == [5, 4, 3, 2, 1]
+    assert listnode_to_list(s.reverseList(list_to_listnode([1, 2]))) == [2, 1]
+    assert listnode_to_list(s.reverseList(list_to_listnode([]))) == []
+
+
+def test_solution():
+    check_cases(Solution())
+
+
+def test_solution_iterative():
+    check_cases(SolutionIterative())
+
+
+def test_solution_iterative2():
+    check_cases(SolutionIterative2())
+
+
+def test_solution_recursive():
+    check_cases(SolutionRecursive())
+
+
+def test_solution_recursive2():
+    check_cases(SolutionRecursive2())
