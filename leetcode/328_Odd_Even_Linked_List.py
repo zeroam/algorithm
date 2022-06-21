899e74a47f787a749ac4370d2694be5bbe94bf5a
jayone <imdff0803@gmail.com>
2022-06-21 13:17:08 +0900

    leetcode - 328. Odd Even Linked List
    - update check solution

--- leetcode/328_Odd_Even_Linked_List.py
+++ leetcode/328_Odd_Even_Linked_List.py
@@ -1,4 +1,4 @@
-from typing import List
+from tabnanny import check
 from common.node import ListNode, listnode_to_list, list_to_listnode
 
 
@@ -21,29 +21,27 @@ class Solution:
         return head
 
 
-def check_solution(head: List[int], expect: List[int]):
-    s = Solution()
-
-    assert listnode_to_list(s.oddEvenList(list_to_listnode(head))) == expect
-
-
-if __name__ == "__main__":
+def check_cases(s: Solution):
     head = []
     expect = []
-    check_solution(head, expect)
+    assert listnode_to_list(s.oddEvenList(list_to_listnode(head))) == expect
 
     head = [1]
     expect = [1]
-    check_solution(head, expect)
+    assert listnode_to_list(s.oddEvenList(list_to_listnode(head))) == expect
 
     head = [1, 2]
     expect = [1, 2]
-    check_solution(head, expect)
+    assert listnode_to_list(s.oddEvenList(list_to_listnode(head))) == expect
 
     head = [1, 2, 3]
     expect = [1, 3, 2]
-    check_solution(head, expect)
+    assert listnode_to_list(s.oddEvenList(list_to_listnode(head))) == expect
 
     head = [1, 2, 3, 4, 5, 6]
     expect = [1, 3, 5, 2, 4, 6]
-    check_solution(head, expect)
+    assert listnode_to_list(s.oddEvenList(list_to_listnode(head))) == expect
+
+
+def test_solution():
+    check_cases(Solution())
