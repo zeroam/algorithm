5a3b58a8f70c9c3c40cd0aec8175b50dca7ffc0b
jayone <imdff0803@gmail.com>
2022-06-23 18:25:20 +0900

    leetcode - 20. Valid Parentheses
    - add solution

--- leetcode/20_Valid_Parentheses.py
+++ leetcode/20_Valid_Parentheses.py
@@ -18,14 +18,35 @@ class Solution:
         return len(stack) == 0
 
 
+class Solution2:
+    def isValid(self, s: str) -> bool:
+        table = {")": "(", "]": "[", "}": "{"}
+        stack = []
+        for char in s:
+            if char not in table:
+                stack.append(char)
+            elif not stack or table[char] != stack.pop():
+                return False
+
+        return len(stack) == 0
+
+
 def check_solutions(ss: str, expect: bool):
     s = Solution()
 
     assert s.isValid(ss) == expect
 
 
-if __name__ == "__main__":
-    check_solutions("()[]{}", True)
-    check_solutions("(", False)
-    check_solutions(")", False)
-    check_solutions("(())[]{([])}", True)
+def check_cases(s: Solution):
+    assert s.isValid("()[]{}") is True
+    assert s.isValid("(") is False
+    assert s.isValid(")") is False
+    assert s.isValid("(())[]{([])}") is True
+
+
+def test_solution():
+    check_cases(Solution())
+
+
+def test_solution2():
+    check_cases(Solution2())
