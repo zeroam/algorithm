29a0a01d67d5575a8f95849499d742a156eed93e
jayone <imdff0803@gmail.com>
2022-07-09 15:22:40 +0900

    leetcode - 743. Network Delay Time
    - add solution priority queue

--- leetcode/743_Network_Delay_Time.py
+++ leetcode/743_Network_Delay_Time.py
@@ -11,14 +11,14 @@ class Solution:
             graph[x - 1].append((y - 1, cost))
 
         visited = [False] * n
-        costs = [float('inf')] * n
+        costs = [float("inf")] * n
         costs[k - 1] = 0
 
         # traverse and update cost
         while True:
             # find cand_node
             cand_node = -1
-            cand_dist = float('inf')
+            cand_dist = float("inf")
             for i in range(n):
                 if visited[i]:
                     continue
@@ -36,7 +36,7 @@ class Solution:
                 costs[node] = min(costs[node], costs[cand_node] + cost)
 
         max_cost = max(costs)
-        return max_cost if max_cost < float('inf') else -1
+        return max_cost if max_cost < float("inf") else -1
 
 
 class SolutionPriorityQueue:
@@ -65,12 +65,39 @@ class SolutionPriorityQueue:
         return max(dist.values()) if len(dist) == n else -1
 
 
+class SolutionPriorityQueue2:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        graph = defaultdict(list)
+        for u, v, w in times:
+            graph[u].append((v, w))
+
+        dist = {}
+        queue = [(0, k)]
+        while queue:
+            time, node = heapq.heappop(queue)
+            if node in dist:
+                continue
+
+            dist[node] = time
+            for v, w in graph[node]:
+                heapq.heappush(queue, (time + w, v))
+
+        if len(dist) != n:
+            return -1
+        return max(dist.values())
+
+
 def check_cases(s: Solution):
     assert s.networkDelayTime([[2, 1, 1], [2, 3, 1], [3, 4, 1]], 4, 2) == 2
     assert s.networkDelayTime([[1, 2, 1]], 2, 1) == 1
     assert s.networkDelayTime([[1, 2, 1]], 2, 2) == -1
     assert s.networkDelayTime([[1, 2, 1], [2, 3, 2]], 3, 1) == 3
-    assert s.networkDelayTime([[1, 2, 1], [1, 3, 1], [1, 4, 3], [2, 4, 1], [3, 5, 1], [4, 5, 2]], 5, 1) == 2
+    assert (
+        s.networkDelayTime(
+            [[1, 2, 1], [1, 3, 1], [1, 4, 3], [2, 4, 1], [3, 5, 1], [4, 5, 2]], 5, 1
+        )
+        == 2
+    )
 
 
 def test_solution():
@@ -79,3 +106,7 @@ def test_solution():
 
 def test_solution_priority_queue():
     check_cases(SolutionPriorityQueue())
+
+
+def test_solution_priority_queue2():
+    check_cases(SolutionPriorityQueue2())
