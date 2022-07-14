4c3b38c2f3a3ecacffc707a73e6fa7f1cf25d931
jayone <imdff0803@gmail.com>
2022-07-14 11:27:02 +0900

    leetcode - 787. Cheapest Flights Within K Stops
    - add solution timelimit exceed, dijkstra

--- leetcode/787_Cheapest_Flights_Within_K_Stops.py
+++ leetcode/787_Cheapest_Flights_Within_K_Stops.py
@@ -4,27 +4,34 @@ from typing import List
 
 
 class Solution:
-    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+    def findCheapestPrice(
+        self, n: int, flights: List[List[int]], src: int, dst: int, k: int
+    ) -> int:
         # make graph
         graph = defaultdict(list)
         for u, v, w in flights:
             graph[v].append((u, w))
 
         # make table
-        price_table = [[float('inf')] * n for _ in range(k + 2)]
+        price_table = [[float("inf")] * n for _ in range(k + 2)]
         for i in range(k + 1):
             price_table[i][src] = 0
 
         for edge in range(1, k + 2):
             for dst_node in range(n):
                 for src_node, price in graph[dst_node]:
-                    price_table[edge][dst_node] = min(price_table[edge][dst_node], price_table[edge -1][src_node] + price)
+                    price_table[edge][dst_node] = min(
+                        price_table[edge][dst_node],
+                        price_table[edge - 1][src_node] + price,
+                    )
 
-        return price_table[k + 1][dst] if price_table[k + 1][dst] < float('inf') else -1
+        return price_table[k + 1][dst] if price_table[k + 1][dst] < float("inf") else -1
 
 
 class SolutionDijkstra:
-    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+    def findCheapestPrice(
+        self, n: int, flights: List[List[int]], src: int, dst: int, k: int
+    ) -> int:
         # make graph
         graph = defaultdict(list)
         for u, v, w in flights:
@@ -32,8 +39,8 @@ class SolutionDijkstra:
 
         # initialize
         hq = [(0, src, -1)]
-        prices = [float('inf')] * n
-        current_stops = [float('inf')] * n
+        prices = [float("inf")] * n
+        current_stops = [float("inf")] * n
         prices[src], current_stops[src] = 0, 0
 
         while hq:
@@ -57,11 +64,13 @@ class SolutionDijkstra:
 
                 current_stops[next_node] = stop
 
-        return prices[dst] if prices[dst] != float('inf') else -1
+        return prices[dst] if prices[dst] != float("inf") else -1
 
 
 class SolutionDFS:
-    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+    def findCheapestPrice(
+        self, n: int, flights: List[List[int]], src: int, dst: int, k: int
+    ) -> int:
         graph = defaultdict(list)
         for u, v, price in flights:
             graph[u].append((v, price))
@@ -73,12 +82,12 @@ class SolutionDFS:
                 return 0
 
             if stop == k + 1:
-                return float('inf')
+                return float("inf")
 
             if (node, stop) in prices:
                 return prices[(node, stop)]
 
-            ans = float('inf')
+            ans = float("inf")
             for next_node, next_price in graph[node]:
                 ans = min(ans, traverse(next_node, stop + 1) + next_price)
 
@@ -86,18 +95,19 @@ class SolutionDFS:
             return ans
 
         ans = traverse(src, 0)
-        return ans if ans != float('inf') else -1
+        return ans if ans != float("inf") else -1
 
 
 class SolutionBellmanFord:
-    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+    def findCheapestPrice(
+        self, n: int, flights: List[List[int]], src: int, dst: int, k: int
+    ) -> int:
         graph = defaultdict(list)
         for u, v, price in flights:
             graph[v].append((u, price))
 
-
         # bellman ford algorithm
-        cur_routes = [float('inf')] * n
+        cur_routes = [float("inf")] * n
         cur_routes[src] = 0
 
         for _ in range(k + 1):
@@ -108,11 +118,13 @@ class SolutionBellmanFord:
                 cur_routes[v] = min(cur_routes[v], prev_routes[u] + price)
 
         ans = cur_routes[dst]
-        return ans if ans != float('inf') else -1
+        return ans if ans != float("inf") else -1
 
 
 class SolutionBFS:
-    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+    def findCheapestPrice(
+        self, n: int, flights: List[List[int]], src: int, dst: int, k: int
+    ) -> int:
         adj_matrix = [[0] * n for _ in range(n)]
         for u, v, w in flights:
             adj_matrix[u][v] = w
@@ -123,7 +135,7 @@ class SolutionBFS:
         queue = deque([src])
 
         stops = 0
-        ans = float('inf')
+        ans = float("inf")
 
         while queue and stops < k + 1:
             for _ in range(len(queue)):
@@ -133,8 +145,8 @@ class SolutionBFS:
                     if adj_matrix[node][nei] == 0:
                         continue
 
-                    d_u = distances.get((node, stops), float('inf'))
-                    d_v = distances.get((nei, stops + 1), float('inf'))
+                    d_u = distances.get((node, stops), float("inf"))
+                    d_v = distances.get((nei, stops + 1), float("inf"))
                     w_uv = adj_matrix[node][nei]
 
                     if stops == k and nei != dst:
@@ -149,33 +161,96 @@ class SolutionBFS:
 
             stops += 1
 
-        return ans if ans != float('inf') else -1
+        return ans if ans != float("inf") else -1
+
+
+class SolutionTimeLimit:
+    def findCheapestPrice(
+        self, n: int, flights: List[List[int]], src: int, dst: int, k: int
+    ) -> int:
+        graph = defaultdict(list)
+        for u, v, w in flights:
+            graph[u].append((v, w))
+
+        visited = set()
+        result = float("inf")
+
+        def dfs(node: int, weight: int, count: int):
+            nonlocal result
+            if node in visited:
+                return
+            if node == dst:
+                result = min(result, weight)
+                return
+            if count == 0:
+                return
+
+            visited.add(node)
+            for v, w in graph[node]:
+                dfs(v, weight + w, count - 1)
+            visited.remove(node)
+
+            return result
+
+        dfs(src, 0, k + 1)
+        return result if result != float("inf") else -1
+
+
+class SolutionDijkstra:
+    def findCheapestPrice(
+        self, n: int, flights: List[List[int]], src: int, dst: int, k: int
+    ) -> int:
+        graph = defaultdict(list)
+        for u, v, w in flights:
+            graph[u].append((v, w))
+
+        queue = [(0, src, k)]
+
+        while queue:
+            price, node, stop = heapq.heappop(queue)
+            if node == dst:
+                return price
+            if stop >= 0:
+                for v, w in graph[node]:
+                    alt = price + w
+                    heapq.heappush(queue, (alt, v, stop - 1))
+        return -1
 
 
 def check_cases(s: Solution):
     n = 5
-    flights = [[1,2,10],[2,0,7],[1,3,8],[4,0,10],[3,4,2],[4,2,10],[0,3,3],[3,1,6],[2,4,5]]
+    flights = [
+        [1, 2, 10],
+        [2, 0, 7],
+        [1, 3, 8],
+        [4, 0, 10],
+        [3, 4, 2],
+        [4, 2, 10],
+        [0, 3, 3],
+        [3, 1, 6],
+        [2, 4, 5],
+    ]
     src = 0
     dst = 4
     k = 1
     assert s.findCheapestPrice(n, flights, src, dst, k) == 5
 
     n = 3
-    flights = [[0,1,2],[1,2,1],[2,0,10]]
+    flights = [[0, 1, 2], [1, 2, 1], [2, 0, 10]]
     src = 1
     dst = 2
     k = 1
     assert s.findCheapestPrice(n, flights, src, dst, k) == 1
 
     n = 4
-    flights = [[0,1,1],[0,2,5],[1,2,1],[2,3,1]]
+    flights = [[0, 1, 1], [0, 2, 5], [1, 2, 1], [2, 3, 1]]
     src = 0
     dst = 3
     k = 1
     assert s.findCheapestPrice(n, flights, src, dst, k) == 6
 
     n = 3
-    flights = [[0,1,100],[1,2,100],[0,2,500]]
+    flights = [[0, 1, 100], [1, 2, 100], [0, 2, 500]]
     src = 0
     dst = 2
     k = 1
@@ -200,3 +275,11 @@ def test_solution_bellman_ford():
 
 def test_solution_bfs():
     check_cases(SolutionBFS())
+
+
+def test_solution_timelimit():
+    check_cases(SolutionTimeLimit())
+
+
+def test_solution_dijkstra():
+    check_cases(SolutionDijkstra())
