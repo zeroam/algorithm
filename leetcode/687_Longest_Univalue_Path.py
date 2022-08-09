93e4b6936b69ed8254edb53de7a800415d60ca00
jayone <imdff0803@gmail.com>
2022-08-09 14:43:23 +0900

    leetcode - 687. Longest Univalue Path
    - add solution

--- leetcode/687_Longest_Univalue_Path.py
+++ leetcode/687_Longest_Univalue_Path.py
@@ -31,6 +31,36 @@ class Solution:
         return result
 
 
+class SolutionDFS:
+    def longestUnivaluePath(self, root: Optional[TreeNode]) -> int:
+        result = 0
+
+        def dfs(node: TreeNode, parent: TreeNode = None):
+            if node is None:
+                return 0
+
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            if node.left and node.left.val == node.val:
+                left += 1
+            else:
+                left = 0
+
+            if node.right and node.right.val == node.val:
+                right += 1
+            else:
+                right = 0
+
+            nonlocal result
+            result = max(result, left + right)
+
+            return max(left, right)
+
+        dfs(root)
+        return result
+
+
 def check_cases(s: Solution):
     assert s.longestUnivaluePath(make_tree_node([5, 4, 5, 1, 1, None, 5])) == 2
     assert s.longestUnivaluePath(make_tree_node([1, 4, 5, 4, 4, None, 5])) == 2
@@ -39,3 +69,7 @@ def check_cases(s: Solution):
 
 def test_solution():
     check_cases(Solution())
+
+
+def test_solution_dfs():
+    check_cases(SolutionDFS())
