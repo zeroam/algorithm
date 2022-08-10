cea879c8e3cc5fcdbae5695eeb695c5a1a9cf892
jayone <imdff0803@gmail.com>
2022-08-10 11:00:25 +0900

    leetcode - 226. Invert Binary Tree
    - add recursive, bfs, dfs solution

--- leetcode/226_Invert_Binary_Tree.py
+++ leetcode/226_Invert_Binary_Tree.py
@@ -1,5 +1,7 @@
-from common.node import TreeNode, make_tree_node, compare_tree_node
+from collections import deque
 from typing import Optional
+
+from common.node import TreeNode, make_tree_node, compare_tree_node
 
 
 class Solution:
@@ -10,6 +12,53 @@ class Solution:
         root.left, root.right = root.right, root.left
         self.invertTree(root.left)
         self.invertTree(root.right)
+
+        return root
+
+
+class SolutionRecursive:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None:
+            return root
+
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+
+        return root
+
+
+class SolutionBFS:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None:
+            return None
+
+        queue = deque([root])
+
+        while queue:
+            node = queue.popleft()
+
+            node.left, node.right = node.right, node.left
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+
+        return root
+
+
+class SolutionDFS:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None:
+            return None
+
+        stack = [root]
+
+        while stack:
+            node = stack.pop()
+            node.left, node.right = node.right, node.left
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
 
         return root
 
@@ -28,3 +77,15 @@ def check_cases(s: Solution):
 
 def test_solution():
     check_cases(Solution())
+
+
+def test_solution_recursive():
+    check_cases(SolutionRecursive())
+
+
+def test_solution_bfs():
+    check_cases(SolutionBFS())
+
+
+def test_solution_dfs():
+    check_cases(SolutionDFS())
